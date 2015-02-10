bc4851832e2f87ee954d1f815afc513331ff3310
taku <okanotaku@gmail.com>
2015-02-10 15:43:23 -0800

    init

---
+++ nav.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+
+<body>
+	<h3 class="select">Select a Design:</h3>
+		<nav role="navigation">
+			<ul>
+				<li><a href="link" class="portfolio">Portfolio Piece 1</a></li>
+                <li><a href="link" class="portfolio">Portfolio Piece 2</a></li>
+                <li><a href="link" class="portfolio">Portfolio Piece 3</a></li>
+                <li><a href="link" class="portfolio">Portfolio Piece 4</a></li>
+                <li><a href="link" class="portfolio">Portfolio Piece 5</a></li>
+                <li><a href="link" class="portfolio">Portfolio Piece 6</a></li>
+                <li><a href="link" class="portfolio">Portfolio Piece 7</a></li>
+                <li><a href="link" class="portfolio">Portfolio Piece 8</a></li>
+                <li><a href="link" class="portfolio">Portfolio Piece 9</a></li>
+                <li><a href="link" class="portfolio">Portfolio Piece 10</a></li>
+            </ul>
+		</nav>
+
+</body>
+</html>
